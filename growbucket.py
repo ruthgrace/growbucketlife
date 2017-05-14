20d301849b49b7e4beaf3d480228d44e438deada
Ruth Grace Wong <ruthgracewong@gmail.com>
2017-05-14 07:14:59 +0000

    flask in nginx

--- growbucket.py
+++ growbucket.py
@@ -6,13 +6,16 @@ def home():
     return render_template('index.html')
 
 @app.route('/faq')
+@app.route('/faq.html')
 def faq():
     return render_template('faq.html')
 
 @app.route('/documents')
+@app.route('/documents.html')
 def documents():
     return render_template('documents.html')
 
 @app.route('/instructable')
+@app.route('/instructable.html')
 def instructable():
     return redirect("https://www.instructables.com/id/Grow-Anything-Grow-Bucket-for-50/", code=302)
