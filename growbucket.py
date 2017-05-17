49bddb4a9c815975858feeec6ecf2f6a7682bdef
Ruth Grace Wong <ruthgracewong@gmail.com>
2017-05-17 07:50:52 +0000

    add payment pages

--- growbucket.py
+++ growbucket.py
@@ -1,9 +1,11 @@
 import logging
+import stripe
 from logging.handlers import RotatingFileHandler
 from flask import Flask, render_template, redirect, request
-from config import testkey, prodkey
+from config import testkey, prodkey, testsecretkey, prodsecretkey
 
-KEY = testkey
+KEY = prodkey
+SECRET = prodsecretkey
 QUANTITIES = [1, 2, 3, 4, 5]
 
 app = Flask(__name__)
@@ -11,10 +13,10 @@ handler = RotatingFileHandler('/var/log/growbucket/app.log', maxBytes=10000, bac
 handler.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
 
-def get_numbers(quantity):
+def get_numbers(quantity, cost):
     numbers = {}
     numbers['quantity'] = quantity
-    numbers['cost'] = 50 * numbers['quantity']
+    numbers['cost'] = cost * numbers['quantity']
     numbers['taxtotalcents'] = (85 * numbers['cost']) / 10
     numbers['taxcents'] = numbers['taxtotalcents'] % 100
     numbers['taxdollars'] = numbers['taxtotalcents'] / 100
@@ -40,7 +42,7 @@ def documents():
 @app.route('/payment', methods = ['GET', 'POST'])
 def payment():
     if request.method == 'GET':
-        numbers = get_numbers(1)
+        numbers = get_numbers(1, 50)
         return render_template('payment.html',
                                key=KEY,
                                quantities=QUANTITIES,
@@ -52,8 +54,62 @@ def payment():
                                totaldollars=numbers['totaldollars'],
                                stripetotal=numbers['stripetotal'])
     if request.method == 'POST':
-        numbers = get_numbers(int(request.form.get('quantity')))
+        numbers = get_numbers(int(request.form.get('quantity')), 50)
         return render_template('payment.html',
+                               key=KEY,
+                               quantities=QUANTITIES,
+                               quantity=numbers['quantity'],
+                               cost=numbers['cost'],
+                               taxcents=numbers['taxcents'],
+                               taxdollars=numbers['taxdollars'],
+                               totalcents=numbers['totalcents'],
+                               totaldollars=numbers['totaldollars'],
+                               stripetotal=numbers['stripetotal'])
+
+@app.route('/two', methods = ['GET', 'POST'])
+def two_dollars():
+    if request.method == 'GET':
+        numbers = get_numbers(1, 2)
+        return render_template('two.html',
+                               key=KEY,
+                               quantities=QUANTITIES,
+                               quantity=numbers['quantity'],
+                               cost=numbers['cost'],
+                               taxcents=numbers['taxcents'],
+                               taxdollars=numbers['taxdollars'],
+                               totalcents=numbers['totalcents'],
+                               totaldollars=numbers['totaldollars'],
+                               stripetotal=numbers['stripetotal'])
+    if request.method == 'POST':
+        numbers = get_numbers(int(request.form.get('quantity')), 2)
+        return render_template('two.html',
+                               key=KEY,
+                               quantities=QUANTITIES,
+                               quantity=numbers['quantity'],
+                               cost=numbers['cost'],
+                               taxcents=numbers['taxcents'],
+                               taxdollars=numbers['taxdollars'],
+                               totalcents=numbers['totalcents'],
+                               totaldollars=numbers['totaldollars'],
+                               stripetotal=numbers['stripetotal'])
+
+@app.route('/ten', methods = ['GET', 'POST'])
+def ten_dollars():
+    if request.method == 'GET':
+        numbers = get_numbers(1, 10)
+        return render_template('ten.html',
+                               key=KEY,
+                               quantities=QUANTITIES,
+                               quantity=numbers['quantity'],
+                               cost=numbers['cost'],
+                               taxcents=numbers['taxcents'],
+                               taxdollars=numbers['taxdollars'],
+                               totalcents=numbers['totalcents'],
+                               totaldollars=numbers['totaldollars'],
+                               stripetotal=numbers['stripetotal'])
+    if request.method == 'POST':
+        numbers = get_numbers(int(request.form.get('quantity')), 10)
+        return render_template('ten.html',
                                key=KEY,
                                quantities=QUANTITIES,
                                quantity=numbers['quantity'],
@@ -66,7 +122,31 @@ def payment():
 
 @app.route('/checkout', methods = ['POST'])
 def checkout():
+    token = request.form.get('stripeToken')
+    amount = request.form.get('stripetotal')
+    stripe.api_key = SECRET
+    charge = stripe.Charge.create(
+      amount=str(amount),
+      description="Grow Bucket Kit",
+      currency="usd",
+      receipt_email=request.form.get('stripeEmail'),
+      source=token
+    )
     return render_template('checkout.html')
+
+@app.route('/donate', methods = ['POST'])
+def donate():
+    token = request.form.get('stripeToken')
+    amount = request.form.get('stripetotal')
+    stripe.api_key = SECRET
+    charge = stripe.Charge.create(
+      amount=str(amount),
+      description="Grow Bucket Donation",
+      currency="usd",
+      receipt_email=request.form.get('stripeEmail'),
+      source=token
+    )
+    return render_template('donate.html')
 
 @app.route('/instructable')
 @app.route('/instructable.html')
